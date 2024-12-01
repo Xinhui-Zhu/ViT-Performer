420e9764b8444cd3682a5c8bd3b02735f9ce34a7
xxiaouw <xx2448@columbia.edu>
2024-11-30 20:11:32 -0800

    random_features for faster performer
    - Main vit-performer py file updated (unified transformer/performer model, using performer-pytorch only; added additional important model parameters that could be changed in config)
    - CIFAR10 Config updated corresponding to main function

--- vit_performer/vit_performer.py
+++ vit_performer/vit_performer.py
@@ -22,6 +22,7 @@ def load_config(config_file):
     return config
 
 def main():
+
     # Argument for config file
     parser = argparse.ArgumentParser(description='PyTorch ViT Example with Config')
     parser.add_argument('--config', type=str, required=True, help='Path to config file')
@@ -64,10 +65,10 @@ def main():
     else:
         dataset_class = getattr(datasets, dataset_name)
         if dataset_name == "Places365":
-            train_dataset = dataset_class('./data', split="train-standard", small=config['dataset']['small'], transform=transform)
-            test_dataset = dataset_class('./data', split="val", small=config['dataset']['small'], transform=transform)
+            train_dataset = dataset_class('./data', download=False, split="train-standard", small=config['dataset']['small'], transform=transform)
+            test_dataset = dataset_class('./data', download=False, split="val", small=config['dataset']['small'], transform=transform)
         else:
-            train_dataset = dataset_class('./data', train=True, transform=transform)
+            train_dataset = dataset_class('./data', download=True, train=True, transform=transform)
             test_dataset = dataset_class('./data', train=False, transform=transform)
 
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=config['train']['batch_size'], shuffle=True)
@@ -84,11 +85,60 @@ def main():
         "LeakyReLU": nn.LeakyReLU(),
         "GELU": nn.GELU(),
         "ELU": nn.ELU(),
+        "Softmax": nn.Softmax()
     }
-    kernel_function = kernel_fn_dict.get(config['model'].get('kernel_fn', 'ReLU'))
+    
+    kernel_function = kernel_fn_dict.get(config['model'].get('kernel_fn', 'Softmax'))
 
     if config['model'].get('use_performer', True):
         print("Using ViT-Performer now...")
+        print(f"Using {config['model']['kernel_fn']}-Performer now...")
+        if config['model'].get('kernel_fn', "Softmax"):
+            performer = Performer(
+                dim=config['model']['model_dim'],
+                depth=config['model']['model_depth'],
+                heads=config['model']['heads'],
+                dim_head=config['model']['dim_head'],
+                causal=False,
+                generalized_attention=False,  # to trigger kernel use in https://github.com/lucidrains/performer-pytorch/blob/fc8b78441b1e27eb5d9b01fc738a8772cee07127/performer_pytorch/performer_pytorch.py#L263
+                proj_type=config['model']['proj_type'],  # should be one of ['disabled', 'default', 'learnable']
+                auto_check_redraw=(config['model']['proj_type'] == 'default'),  # do not activate auto redraw if proj matrix is learned or disabled
+                ff_dropout = config['model']['dropout'],
+                attn_dropout = config['model']['dropout'],
+                feature_redraw_interval = config['model']['redraw_interval'],
+                nb_features = config['model']['random_features'],
+                ff_mult = config['model']['feedforward_dim_multiplier'],
+                qkv_bias = True,
+                no_projection = False, # False turns performer into transformer
+            ) 
+        else:
+            performer = Performer(
+                dim=config['model']['model_dim'],
+                depth=config['model']['model_depth'],
+                heads=config['model']['heads'],
+                dim_head=config['model']['dim_head'],
+                causal=False,
+                generalized_attention=True,  # to trigger kernel use in https://github.com/lucidrains/performer-pytorch/blob/fc8b78441b1e27eb5d9b01fc738a8772cee07127/performer_pytorch/performer_pytorch.py#L263
+                kernel_fn=kernel_function,
+                proj_type=config['model']['proj_type'],  # should be one of ['disabled', 'default', 'learnable']
+                auto_check_redraw=(config['model']['proj_type'] == 'default'),  # do not activate auto redraw if proj matrix is learned or disabled
+                ff_dropout = config['model']['dropout'],
+                attn_dropout = config['model']['dropout'],
+                feature_redraw_interval = config['model']['redraw_interval'],
+                nb_features = config['model']['random_features'],
+                ff_mult = config['model']['feedforward_dim_multiplier'],
+                qkv_bias = True,
+                no_projection = False, # False turns performer into transformer
+            ) 
+        model = ViTHacked(
+            dim=config['model']['model_dim'],
+            image_size=config['model']['image_size'],
+            patch_size=config['model']['patch_size'],
+            num_classes=config['model']['num_classes'],
+            transformer=performer
+        )
+    else:
+        print("Using ViT-Transformer now...")
         performer = Performer(
             dim=config['model']['model_dim'],
             depth=config['model']['model_depth'],
@@ -101,6 +151,11 @@ def main():
             kernel_fn=kernel_function,
             ff_dropout = config['model']['dropout'],
             attn_dropout = config['model']['dropout'],
+            feature_redraw_interval = config['model']['redraw_interval'],
+            nb_features = config['model']['random_features'],
+            ff_mult = config['model']['feedforward_dim_multiplier'],
+            qkv_bias = True,
+            no_projection = True,
         )
         model = ViTHacked(
             dim=config['model']['model_dim'],
@@ -109,20 +164,13 @@ def main():
             num_classes=config['model']['num_classes'],
             transformer=performer
         )
-    else:
-        print("Using normal ViT now...\n")
-        model = vit_pytorch.ViT(
-            image_size = config['model']['image_size'],
-            patch_size = config['model']['patch_size'],
-            num_classes = config['model']['num_classes'],
-            dim = config['model']['model_dim'],
-            depth = config['model']['model_depth'],
-            heads = config['model']['heads'],
-            mlp_dim = config['model']['mlp_dim'],
-            dropout = config['model']['dropout'],
-            emb_dropout = config['model']['dropout'],
-        )
 
+    print("Printing out the model structure:")
+    print("Note: The model structure may appear repeated inside 'proj_updater'.")
+    print("This is intentional and does not affect the model's functionality or computational complexity.")
+    print("The repeated structure is for managing projection updates and is only a reference to previous structure.")
+    print(model)
+    
     # Move model to device
     model = model.to(device)
 
@@ -149,7 +197,7 @@ def main():
     ttl_train_duration, ttl_test_duration = 0, 0
     for epoch in range(1, config['train']['epochs'] + 1):
         ttl_train_duration += train(config, model, device, train_loader, optimizer, epoch)
-        ttl_test_duration += test(config, model, device, test_loader)
+        ttl_test_duration += test(config, model, device, test_loader, epoch, config['model'].get('use_performer', True))
         scheduler.step()
 
     log_train_speed = math.log2(ttl_train_duration / len(train_loader.dataset) / epoch)
@@ -195,47 +243,73 @@ def train(config, model, device, train_loader, optimizer, epoch):
     train_duration = end_time - start_time  
     print(f'Epoch {epoch} | ', end="")
     return train_duration
-        
-def test(config, model, device, test_loader):
+
+def test(config, model, device, test_loader, epoch, performer):
     model.eval()
     test_loss = 0
     correct = 0
-    test_frob_errs = 0
-    start_time = time.time() 
+    start_time = time.time()
+    
+    # attention_matrix_saved = False  # Flag to check if attention matrix is already saved
+
+    # # Function to save the attention matrix
+    # def save_attention_matrix(module, input, output):
+    #     nonlocal attention_matrix_saved  # Access the flag from outer scope
+    #     if not attention_matrix_saved:
+    #         attention_matrix = output[1].detach().cpu()
+    #         # Save the attention matrix to a file
+    #         os.makedirs("output", exist_ok=True)
+    #         torch.save(attention_matrix, os.path.join("output", f"{epoch}_attention_matrix.pt"))
+    #         attention_matrix_saved = True  # Set flag to True after saving once
+    
+    # if performer:
+    #     # Register hooks on each SelfAttention layer in the model
+    #     for layer in model.transformer.net.layers:  # Access layers within transformer.net
+    #         if isinstance(layer, torch.nn.ModuleList):
+    #             for sublayer in layer:
+    #                 # Check if the layer contains SelfAttention
+    #                 if isinstance(sublayer.fn, torch.nn.Module) and hasattr(sublayer.fn, 'fast_attention'):
+    #                     sublayer.fn.register_forward_hook(save_attention_matrix)
+    # else:
+    #     # Register hook on each Attention layer within the transformer
+    #     for layer in model.transformer.layers:
+    #         # Each layer is a ModuleList with [Attention, FeedForward]
+    #         layer[0].attend.register_forward_hook(save_attention_matrix)
+
     with torch.no_grad():
         for batch in test_loader:
+            # Prepare data and target
             if "imagenet" in config['dataset']['name'].lower():
                 target = batch['label'].clone().detach().long()
                 data = batch['image']
                 data = torch.stack([torch.stack([torch.stack([x.clone().detach() for x in row]) for row in channel]) for channel in data])
-                data = data.permute(3, 0, 1, 2).float()  
+                data = data.permute(3, 0, 1, 2).float()
             else:
                 data, target = batch
             data, target = data.to(device), target.to(device)
             data = data.expand(-1, 3, -1, -1)
+            
+            # Forward pass through the model
             output = model(data)
-            if isinstance(output, tuple):
-                output, frob_errs = output
-                # TODO: calc aggregated final mean Frobenius Error across layers of attn and across batch
-                # test_frob_errs += ...
             output = F.log_softmax(output, dim=1)
             test_loss += F.nll_loss(output, target, reduction='sum').item()
             pred = output.argmax(dim=1, keepdim=True)
             correct += pred.eq(target.view_as(pred)).sum().item()
 
-    end_time = time.time()  
-    test_duration = end_time - start_time 
-
+    # Calculate and print test statistics
+    end_time = time.time()
+    test_duration = end_time - start_time
     test_loss /= len(test_loader.dataset)
-    test_frob_errs /= len(test_loader.dataset)
     accuracy = 100. * correct / len(test_loader.dataset)
     print(f'Test set: Average loss: {test_loss:.4f}, Accuracy: {correct}/{len(test_loader.dataset)} ({accuracy:.0f}%)\n')
+
+    # Log to wandb if enabled
     if config['wandb']['use_wandb']:
         wandb.log({
             "test_loss": test_loss,
-            "test_frob_errs": test_frob_errs,
             "test_accuracy": accuracy,
         })
+
     return test_duration
 
 if __name__ == '__main__':
